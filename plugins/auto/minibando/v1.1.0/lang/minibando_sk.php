f20ea917ca164ba7deeeee2f076e9c83d33b8830
phenix <trash@henix.be>
2013-02-10 23:21:07 +0100

    Multilang et crayons + adaptation des squelette

---
+++ plugins/auto/minibando/v1.1.0/lang/minibando_sk.php
@@ -0,0 +1,20 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/minibando?lang_cible=sk
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'mode_debug' => 'Režim ladenia',
+	'mode_inclure' => 'Režim vkladania',
+	'mode_profile' => 'Režim profilovania',
+
+	// T
+	'titre_debug' => 'Ladiť',
+	'titre_outils_rapides' => 'Vytvorenie'
+);
+
+?>
