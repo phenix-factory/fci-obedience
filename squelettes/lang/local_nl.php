1fd5f199008cb716a89bc6af87e26ef02436caf4
phenix <trash@henix.be>
2013-03-02 16:50:57 +0100

    fix

---
+++ squelettes/lang/local_nl.php
@@ -0,0 +1,33 @@
+<?php 
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	// A
+	// B
+	// C
+	// D
+	// E
+	// F
+	// G
+	// H
+	// I
+	// J
+	'juge' => 'juge'
+	// L
+	// N
+	// M
+	// O
+	// P
+	// Q
+	// R
+	// S
+	// T
+	// U
+	// V
+	// W
+	// X
+	// Y
+	// Z
+);
+
+?>
